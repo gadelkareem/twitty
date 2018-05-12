d0792cf6433f8da8d7ae0842fe71c7f4dde40f20
Waleed Gadelkareem <gadelkareem@gmail.com>
2018-05-12 14:13:15 +0200

    add cache

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Twitty
 [![Build Status](https://travis-ci.org/gadelkareem/twitty.svg)](https://travis-ci.org/gadelkareem/twitty)
  # Installation
-- fill and rename .env.dist to .env
+- Fill and rename .env.dist to .env
 - `docker-compose build --no-cache`
 - `docker-compose up`
-- open [http://localhost:8000/](http://localhost:8000)
+- Open [http://localhost:8000/](http://localhost:8000)
  # Run from command line
 `docker-compose exec symfony /app/bin/console wonderkind:count-retweeter-followers -vvv --tweet-url=https://twitter.com/SpaceX/status/995043176363671552`
 
@@ -14,7 +14,6 @@
 
 
  # TODO
-- caching
 - tests
 - pub/sub ?
     
--- src/Service/ReportService.php
+++ src/Service/ReportService.php
@@ -4,7 +4,7 @@ namespace WonderKind\Service;
 
 use Psr\Log\LoggerInterface;
 use WonderKind\Twitter\TwitterClient;
-
+use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 /**
  * Class ReportService
@@ -20,28 +20,46 @@ class ReportService
      * @var TwitterClient
      */
     private $twitter;
+    /*
+     * @var AdapterInterface
+     */
+    private $cache;
 
     /**
      * TwitterClient constructor.
      * @param TwitterClient $client
+     * @param AdapterInterface $cache
      * @param LoggerInterface $logger
      */
-    public function __construct(TwitterClient $client, LoggerInterface $logger)
+    public function __construct(TwitterClient $client, AdapterInterface $cache, LoggerInterface $logger)
     {
         $this->twitter = $client;
         $this->logger = $logger;
+        $this->cache = $cache;
     }
 
 
     /**
      * @param string $url
      * @return int
+     * @throws \Exception
      * @throws \WonderKind\Exception\TwitterException
      */
     public function CountReTweeterFollowers(string $url): int
     {
+        $cacheKey = "CountReTweeterFollowers_" . md5($url);
+        $cacheItem = $this->cache->getItem($cacheKey);
+        if ($cacheItem->isHit()) {
+            return $cacheItem->get();
+        }
+
         $id = $this->twitter->extractTweetIdFromUrl($url);
         $reTweeters = $this->twitter->getReTweeters($id);
-        return $this->twitter->calculateRetweetFollowers($reTweeters);
+        $totalFollowers = $this->twitter->calculateRetweetFollowers($reTweeters);
+
+        $cacheItem->set($totalFollowers)->expiresAfter(new \DateInterval("PT2H"));
+        $this->cache->save($cacheItem);
+
+        return $totalFollowers;
     }
 }
